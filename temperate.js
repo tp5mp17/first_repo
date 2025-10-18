aa9f4e46190b39562ca03561b9be8f4c63e0ced6
tp5mp17 <tp5mp.17@gmail.com>
2025-10-18 01:22:00 -0400

    inventoryManagement.js

---
+++ temperate.js
@@ -0,0 +1,17 @@
+//temperate
+
+//const day1Tempf = 32
+//const day2TempC = 25
+//const day3Tempf = 70
+//const day4TempC = 18
+
+ let tempInFahrenheit = 74
+ let tempInCelsius = (tempInFahrenheit * 9/5) + 32
+    (tempInFahrenheit -32) * 5/9
+    
+
+console.log(tempInFahrenheit)
+
+
+
+
