76faa9c8dcf4ff0ea933d3642935a6dcdc21c640
tp5mp17 <tp5mp.17@gmail.com>
2025-10-20 01:39:57 -0400

    gardenManagement.js

--- temperate.js
+++ temperate.js
@@ -1,16 +1,19 @@
 //temperate
 
-//const day1Tempf = 32
-//const day2TempC = 25
-//const day3Tempf = 70
-//const day4TempC = 18
+const day1Tempf = 32
+const day2TempC = 25
+const day3Tempf = 70
+const day4TempC = 18
 
- let tempInFahrenheit = 74
- let tempInCelsius = (tempInFahrenheit * 9/5) + 32
-    (tempInFahrenheit -32) * 5/9
+const tempInFahrenheit = ["day1Tempf", "day3Tempf"]
+const tempInCelsius = ["day2TempC", "day4TempC"]
+
+
+
+
     
 
-console.log(tempInFahrenheit)
+console.log(tempInFahrenheit, tempInCelsius)
 
 
 
