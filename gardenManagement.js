51f0399b48c721cb0f602dd3e171d3e043dbf4b0
tp5mp17 <tp5mp.17@gmail.com>
2025-10-21 14:20:30 -0400

    gardenmanagement.js

---
+++ gardenManagement.js
@@ -0,0 +1,20 @@
+const temperate = 90
+
+let timeofday = "morning"
+
+let soilMoisture = 30
+
+if (temperate >= 80) {
+    console.log("watering on")
+} else {
+    console.log("watering off")
+}
+
+if (timeofday = "evening") {
+    console.log("lights on")
+} else {
+    console.log("lights off")
+} 
+while (soilMoisture <40) {
+ soilMoisture += 5
+} console.log(soilMoisture)
