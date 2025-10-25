77b54ccc33df19939a0ca56972f8ff0fe250aa73
tp5mp17 <tp5mp.17@gmail.com>
2025-10-25 03:05:55 -0400

    labcalculate.js

---
+++ LabCalculate.js
@@ -0,0 +1,37 @@
+const history = [];
+
+function recordCalculation(operand1, operand2, opertor, result) {
+    history.push({
+        operands: [operand1, operand2],
+        opertor: opertor,
+        result: result
+    });
+}
+
+function add(a, b) {
+    const result = a + b;
+    recordCalculation(a, b, + result);
+    return result;
+}
+
+function subtract(a, b) {
+    const result = a - b;
+    recordCalculation(a, b, - result);
+    return result;
+}
+
+function multiply(a, b) {
+    const result = a * b;
+    recordCalculation(a, b, 10);
+    return result;
+}
+console.log(result)
+
+function divide(a, b) {
+    if (b === 0) {
+        const errorMag = "error: cannot divide by zero";
+        recordCalculation(a, b, errorMag);
+        return errorMag;
+    }
+} 
+console.log(add, subtract, multiply, divide);
