3b4b555dc070e5c1a4ded0c66aa19041bd0a47f3
tp5mp17 <tp5mp.17@gmail.com>
2025-10-25 02:03:35 -0400

    calculateTax.js

--- calculateTax.js
+++ calculateTax.js
@@ -1,7 +1,25 @@
 function calculateTax(amount) {
 return amount * 0.10;
 }
+const purchaseAmount = 100;
+const tax = calculateTax(purchaseAmount);
+//console.log("purchaseAmount", "tax");
+
 function convertToUpperCase(text) {
     return text.toUpperCase();
 }
-function 
+function findMaximum(num1, num2) {
+    return num1 > num2;
+} 
+
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+function calculateDiscountedPrice(originlPrice, discountPercentage) {
+    const discountAmount = originlPrice * (discountPercentage / 100);
+    return originlPrice - discountAmount;
+}
+let DiscountedPrice = calculateDiscountedPrice(100, 20);
+console.log(DiscountedPrice);
