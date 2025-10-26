97c75d6079a255f5acb0a1d1308d6663ff4e4469
tp5mp17 <tp5mp.17@gmail.com>
2025-10-26 03:36:45 -0400

    labevent.js

---
+++ LabEvent.js
@@ -0,0 +1,10 @@
+const attendee = {
+    attendeeId: "t001",
+    name: "Alice Smith",
+    event: "javaScript Conference",
+    ticketType: "VIP",
+    ticketprice: 150.00
+}; 
+function LogAttendeeName(attendee) {
+    console.log(attendee.name);
+}
