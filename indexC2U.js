44caa1c8a18b50e6b12593f3f79381a1e7843028
tp5mp17 <tp5mp.17@gmail.com>
2025-11-22 01:04:22 -0500

    indexC2U.js

--- indexC2U.js
+++ indexC2U.js
@@ -1,5 +1,20 @@
 function combinUesrs(...args) {
     const combinObject = {
     uesrs: []
+    };
+    for (const userArray of args) {
+        combinObject.users = [...combinObject.uesrs, ...userArray];
+
     }
+
+    return combinObject;
 }
+ const A = [{ id: 1, name: 'Alice' }];
+ const B = [{ id: 2, name: 'bill'}]
+
+const finalCombinedData = combinUesrs(A, B);
+console.log(finalCombinedData);
+
+ module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
